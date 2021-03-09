aafbed17e4b1b180bfa178cf44e5d2bb172911fe
Breon Sparrow <beyond.committed@gmail.com>
2021-03-09 08:18:17 -0700

    Updates and README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,18 +5,18 @@ var presentTime = parseInt(today);
 var saveButton = $(".saveBtn");
 
 // giving time blocks attributes classes and time format to be represented by
-$("#6oclock").attr("data-time", moment("6:00 am", "h:mm a").format("HH"));
-$("#7oclock").attr("data-time", moment("7:00 am", "h:mm a").format("HH"));
-$("#8oclock").attr("data-time", moment("8:00 am", "h:mm a").format("HH"));
-$("#9oclock").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
-$("#10oclock").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
-$("#11oclock").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
-$("#12oclock").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
-$("#1oclock").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
-$("#2oclock").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
-$("#3oclock").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
-$("#4oclock").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
-$("#5oclock").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+$("#6oclock").attr("data-clock", moment("6:00 am", "h:mm a").format("HH"));
+$("#7oclock").attr("data-clock", moment("7:00 am", "h:mm a").format("HH"));
+$("#8oclock").attr("data-clock", moment("8:00 am", "h:mm a").format("HH"));
+$("#9oclock").attr("data-clock", moment("9:00 am", "h:mm a").format("HH"));
+$("#10oclock").attr("data-clock", moment("10:00 am", "h:mm a").format("HH"));
+$("#11oclock").attr("data-clock", moment("11:00 am", "h:mm a").format("HH"));
+$("#12oclock").attr("data-clock", moment("12:00 pm", "h:mm a").format("HH"));
+$("#1oclock").attr("data-clock", moment("1:00 pm", "h:mm a").format("HH"));
+$("#2oclock").attr("data-clock", moment("2:00 pm", "h:mm a").format("HH"));
+$("#3oclock").attr("data-clock", moment("3:00 pm", "h:mm a").format("HH"));
+$("#4oclock").attr("data-clock", moment("4:00 pm", "h:mm a").format("HH"));
+$("#5oclock").attr("data-clock", moment("5:00 pm", "h:mm a").format("HH"));
 // renders the values set in the local storage to the page
   $(document).ready(function() {
 renderSchedule();
@@ -26,7 +26,7 @@ renderSchedule();
 
   // for loop cycling through the timeBlocks by checking for the value which is qual to each id
   for (var i = 1; i <= 12; i++) {
-    var timeBlock = $("#" + i + "oclock").attr("data-time");
+    var timeBlock = $("#" + i + "oclock").attr("data-clock");
     var timeBlockInt = parseInt(timeBlock);
 
     // changing the timeblocks color to match with the present past and future criteria based on the comparisons between presentTime and timeBlockInt
@@ -42,8 +42,8 @@ renderSchedule();
   }
 // Listens for the click of the save button
   saveButton.on("click", function () {
-    // variable that will select hourBLock as the key
-    var hourBlock = $(this).attr("data-hour");
+    // variable that will make hourBlock the key
+    var hourBlock = $(this).attr("data-event");
 
     // variable that will be the value of the input 
     var event = $("#" + hourBlock + "oclock").val();
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Day Planner
+## Task:
+```
+Create a day planner where event can be saved for each hour of the day.
+```
+## User Story
+```
+With this day planner I modified code to set events throughout the hours of a normal business day.
+The user will be able to store these events and view them whenever they would like, as long as the event is saved.
+The user will be able to edit and save the data as they would like.
+The daily planner will let the user know which hour is past, present or future.
+This will help any user stay organized and keep track of their day.
+```
+## Acceptance Criteria Met
+```
+GIVEN a user has daily planner to create a schedule
+WHEN user opens the planner
+THEN the current day is displayed at the top of the calendar
+WHEN user scrolls down
+THEN user is presented with timeblocks for standard business hours
+WHEN user views the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN user clicks into a timeblock
+THEN user can enter an event
+WHEN user clicks the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN user refreshes the page
+THEN the saved events persist
+```
+## Mock up
+![Day-Planner Demo](Assets\images\Day-Planner-Screenshot.png)
